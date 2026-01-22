9d50ac7cf7153b06d290a7cc395ae9ee30c40a31
Sancy061 <Yeison.S.Sanchez.-ND@disney.com>
2026-01-22 16:46:15 -0500

    name project update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have the 'my-first-angular-app' title`, () => {
+    it(`should have the 'no-ssr-angular-app' title`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('my-first-angular-app');
+        expect(app.title).toEqual('no-ssr-angular-app');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-angular-app');
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, no-ssr-angular-app');
     });
 });
